150f7421e6be7902c45b028abdae48b154dcb491
Dave Gumba <davegumba@gmail.com>
2024-11-10 12:38:40 -0500

    Initial Commit

---
+++ Fight Watch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fight Watch
+//
+//  Created by Dave Gumba on 2024-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fight Watch/Fight_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fight_WatchApp.swift
+//  Fight Watch
+//
+//  Created by Dave Gumba on 2024-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Fight_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
